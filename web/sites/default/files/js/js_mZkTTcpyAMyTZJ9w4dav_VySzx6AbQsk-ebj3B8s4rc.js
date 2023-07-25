0fc65ad47fb03f0d84597bf15b5b25107ebba3b0
najimaldin katemwa <naj.naj001@gmail.com>
2023-07-26 00:30:09 +0300

    clients added and menu cleanup

---
+++ web/sites/default/files/js/js_mZkTTcpyAMyTZJ9w4dav_VySzx6AbQsk-ebj3B8s4rc.js
@@ -0,0 +1,255 @@
+/**
+ * @file
+ * JavaScript behaviors for details element.
+ */
+
+(function ($, Drupal, once) {
+
+  'use strict';
+
+  // Determine if local storage exists and is enabled.
+  // This approach is copied from Modernizr.
+  // @see https://github.com/Modernizr/Modernizr/blob/c56fb8b09515f629806ca44742932902ac145302/modernizr.js#L696-731
+  var hasLocalStorage = (function () {
+    try {
+      localStorage.setItem('webform', 'webform');
+      localStorage.removeItem('webform');
+      return true;
+    }
+    catch (e) {
+      return false;
+    }
+  }());
+
+  /**
+   * Attach handler to save details open/close state.
+   *
+   * @type {Drupal~behavior}
+   */
+  Drupal.behaviors.webformDetailsSave = {
+    attach: function (context) {
+      if (!hasLocalStorage) {
+        return;
+      }
+
+      // Summary click event handler.
+      $(once('webform-details-summary-save', 'details > summary', context)).on('click', function () {
+        var $details = $(this).parent();
+
+        // @see https://css-tricks.com/snippets/jquery/make-an-jquery-hasattr/
+        if ($details[0].hasAttribute('data-webform-details-nosave')) {
+          return;
+        }
+
+        var name = Drupal.webformDetailsSaveGetName($details);
+        if (!name) {
+          return;
+        }
+
+        var open = ($details.attr('open') !== 'open') ? '1' : '0';
+        localStorage.setItem(name, open);
+      });
+
+      // Initialize details open state via local storage.
+      $(once('webform-details-save', 'details', context)).each(function () {
+        var $details = $(this);
+
+        var name = Drupal.webformDetailsSaveGetName($details);
+        if (!name) {
+          return;
+        }
+
+        var open = localStorage.getItem(name);
+        if (open === null) {
+          return;
+        }
+
+        if (open === '1') {
+          $details.attr('open', 'open');
+        }
+        else {
+          $details.removeAttr('open');
+        }
+      });
+    }
+
+  };
+
+  /**
+   * Get the name used to store the state of details element.
+   *
+   * @param {jQuery} $details
+   *   A details element.
+   *
+   * @return {string}
+   *   The name used to store the state of details element.
+   */
+  Drupal.webformDetailsSaveGetName = function ($details) {
+    if (!hasLocalStorage) {
+      return '';
+    }
+
+    // Ignore details that are vertical tabs pane.
+    if ($details.hasClass('vertical-tabs__pane')) {
+      return '';
+    }
+
+    // Any details element not included a webform must have define its own id.
+    var webformId = $details.attr('data-webform-element-id');
+    if (webformId) {
+      return 'Drupal.webform.' + webformId.replace('--', '.');
+    }
+
+    var detailsId = $details.attr('id');
+    if (!detailsId) {
+      return '';
+    }
+
+    var $form = $details.parents('form');
+    if (!$form.length || !$form.attr('id')) {
+      return '';
+    }
+
+    var formId = $form.attr('id');
+    if (!formId) {
+      return '';
+    }
+
+    // ISSUE: When Drupal renders a webform in a modal dialog it appends a unique
+    // identifier to webform ids and details ids. (i.e. my-form--FeSFISegTUI)
+    // WORKAROUND: Remove the unique id that delimited using double dashes.
+    formId = formId.replace(/--.+?$/, '').replace(/-/g, '_');
+    detailsId = detailsId.replace(/--.+?$/, '').replace(/-/g, '_');
+    return 'Drupal.webform.' + formId + '.' + detailsId;
+  };
+
+})(jQuery, Drupal, once);
+;
+/**
+ * @file
+ * JavaScript behaviors for message element integration.
+ */
+
+(function ($, Drupal, once) {
+
+  'use strict';
+
+  // Determine if local storage exists and is enabled.
+  // This approach is copied from Modernizr.
+  // @see https://github.com/Modernizr/Modernizr/blob/c56fb8b09515f629806ca44742932902ac145302/modernizr.js#L696-731
+  var hasLocalStorage = (function () {
+    try {
+      localStorage.setItem('webform', 'webform');
+      localStorage.removeItem('webform');
+      return true;
+    }
+    catch (e) {
+      return false;
+    }
+  }());
+
+  // Determine if session storage exists and is enabled.
+  // This approach is copied from Modernizr.
+  // @see https://github.com/Modernizr/Modernizr/blob/c56fb8b09515f629806ca44742932902ac145302/modernizr.js#L696-731
+  var hasSessionStorage = (function () {
+    try {
+      sessionStorage.setItem('webform', 'webform');
+      sessionStorage.removeItem('webform');
+      return true;
+    }
+    catch (e) {
+      return false;
+    }
+  }());
+
+  /**
+   * Behavior for handler message close.
+   *
+   * @type {Drupal~behavior}
+   */
+  Drupal.behaviors.webformMessageClose = {
+    attach: function (context) {
+      $(once('webform-message--close', '.js-webform-message--close', context)).each(function () {
+        var $element = $(this);
+
+        var id = $element.attr('data-message-id');
+        var storage = $element.attr('data-message-storage');
+        var effect = $element.attr('data-message-close-effect') || 'hide';
+        switch (effect) {
+          case 'slide': effect = 'slideUp'; break;
+
+          case 'fade': effect = 'fadeOut'; break;
+        }
+
+        // Check storage status.
+        if (isClosed($element, storage, id)) {
+          return;
+        }
+
+        // Only show element if it's style is not set to 'display: none'
+        // and it is not hidden via .js-webform-states-hidden.
+        if ($element.attr('style') !== 'display: none;' && !$element.hasClass('js-webform-states-hidden')) {
+          $element.show();
+        }
+
+        $element.find('.js-webform-message__link').on('click', function (event) {
+          $element[effect]();
+          setClosed($element, storage, id);
+          $element.trigger('close');
+          event.preventDefault();
+        });
+      });
+    }
+  };
+
+  function isClosed($element, storage, id) {
+    if (!id || !storage) {
+      return false;
+    }
+
+    switch (storage) {
+      case 'local':
+        if (hasLocalStorage) {
+          return localStorage.getItem('Drupal.webform.message.' + id) || false;
+        }
+        return false;
+
+      case 'session':
+        if (hasSessionStorage) {
+          return sessionStorage.getItem('Drupal.webform.message.' + id) || false;
+        }
+        return false;
+
+      default:
+        return false;
+    }
+  }
+
+  function setClosed($element, storage, id) {
+    if (!id || !storage) {
+      return;
+    }
+
+    switch (storage) {
+      case 'local':
+        if (hasLocalStorage) {
+          localStorage.setItem('Drupal.webform.message.' + id, true);
+        }
+        break;
+
+      case 'session':
+        if (hasSessionStorage) {
+          sessionStorage.setItem('Drupal.webform.message.' + id, true);
+        }
+        break;
+
+      case 'user':
+      case 'state':
+      case 'custom':
+        $.get($element.find('.js-webform-message__link').attr('href'));
+        return true;
+    }
+  }
+
+})(jQuery, Drupal, once);
+;
